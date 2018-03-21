44875f2d909949dbeb9213ba5977e29633aceea3
shubhs08 <shubhipandey888@gmail.com>
2018-03-21 11:48:49 +0530

    my first commit

---
+++ Arithmetic.java
@@ -0,0 +1,16 @@
+class Arithmetic
+{
+public static void main(String args[])
+{
+int a=5;
+int b=10;
+int c,d,e,f;
+c=a+b;
+System.out.println("addition of two numbers is:"+c);
+d=b-a;
+System.out.println("subtraction of two numbers is:"+d);
+e=a*b;
+System.out.println("multiplication of two numbers is:"+e);
+f=b/a;
+System.out.println("division of two numbers is:"+f);
+}}
